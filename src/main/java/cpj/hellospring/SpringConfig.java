80878f2e34bbd1cc8a95d65479db4b2508b8ba59
CPJ <abzi110@naver.com>
2021-12-19 00:29:58 +0900

    기본 방식의 JDBC를 이용한 DB 연동 및 스프링 통합 테스트 완료
    - build.gradle에 dependencies 추가.
    - SpringConfig 수정.
    - application.properties 값 수정.
    - DB 연동 코드 실행 및 테스트 완료.
    - 추후 JdbcTemplate를 이용한 방식과 JPA를 사용한 방식 실습 예정.

--- src/main/java/cpj/hellospring/SpringConfig.java
+++ src/main/java/cpj/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package cpj.hellospring;
 
+import cpj.hellospring.repository.JdbcMemberRepository;
 import cpj.hellospring.repository.MemberRepository;
-import cpj.hellospring.repository.MemoryMemberRepository;
 import cpj.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
